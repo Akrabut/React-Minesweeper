d3bf79f6ce6bb6af5654f14643616707a383913e
Or <orshadad@gmail.com>
2020-04-03 00:28:18 +0300

    sidebar is now responsive with a toggle button

--- src/components/Sidenavbar.js
+++ src/components/Sidenavbar.js
@@ -1,20 +1,36 @@
-import React from 'react'
-import { Sidebar, Menu } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Sidebar, Menu, Button } from 'semantic-ui-react'
 import Superman from './Superman'
 import NewGameForm from './NewGameForm'
 
-const Sidenavbar = () => (
-  <Sidebar
-    className='bar'
-    as={Menu}
-    animation='overlay'
-    visible
-    inverted
-    vertical
-    style={{ width: '8%' }}>
-    <Superman />
-    <NewGameForm />
-  </Sidebar>
-)
+const Sidenavbar = () => {
+  const [visible, setVisible] = useState(false)
+
+  return (
+    <aside>
+      <Button
+        className='options-button'
+        toggle
+        active={visible}
+        onClick={() => setVisible(!visible)}
+        floated='right'
+        style={{ marginBottom: '2%' }}>
+        Options
+      </Button>
+      <Sidebar
+        className='bar'
+        as={Menu}
+        animation='overlay'
+        onHide={() => setVisible(false)}
+        visible={visible}
+        inverted
+        vertical
+        width='thin'>
+        <Superman />
+        <NewGameForm />
+      </Sidebar>
+    </aside>
+  )
+}
 
 export default Sidenavbar
